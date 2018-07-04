b9d189d4235de68ec7a4e8467996b9f4bfc0758c
pbalawender <przemyslaw.balawender@gmail.com>
2018-07-04 19:14:19 +0200

    reducer tests + App test fix

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,13 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 
 const mockStore = configureMockStore([thunk]);
-const store = mockStore({});
+const store = mockStore({
+    currencies: {
+        all: [],
+        favoriteCodes: [],
+        loading: false
+    }
+});
 describe('App', () => {
 
     it('renders without crashing', () => {
