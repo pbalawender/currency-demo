e9dc2253c76f4064d36456d9c1bbace7412317e1
pbalawender <przemyslaw.balawender@gmail.com>
2018-07-04 19:03:19 +0200

    currencies store + simple list

--- src/containers/FavoriteCurrencies.js
+++ src/containers/FavoriteCurrencies.js
@@ -1,26 +1,45 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import './FavoriteCurrencies.css';
+import {clearFavoriteCurrencies, fetchCurrenciesList, toggleFavoriteCurrency} from "../actions";
 
 class FavoriteCurrencies extends Component {
 
     static propTypes = {
-
+        all: PropTypes.array,
+        favoriteCodes: PropTypes.array,
+        loading: PropTypes.bool,
+        fetchCurrenciesList: PropTypes.func,
+        toggleFavoriteCurrency: PropTypes.func,
+        clearFavoriteCurrencies: PropTypes.func
     };
 
+    componentDidMount() {
+        this.props.fetchCurrenciesList();
+    }
+
     render() {
+        if (this.props.loading) {
+            return "Loading...";
+        }
         return (
             <div className="FavoriteCurrencies">
-                Hello FavoriteCurrencies!
+                {this.props.all.map((c) => <p key={c.code}>{c.code} - {c.currency}</p>)}
             </div>
         );
     }
 }
 
 
-const mapsStateToProps = () => ({
-
+const mapsStateToProps = (state) => ({
+    all: state.currencies.all,
+    favoriteCodes: state.currencies.favoriteCodes,
+    loading: state.currencies.loading
 });
 
 export default connect(mapsStateToProps, {
+    fetchCurrenciesList,
+    toggleFavoriteCurrency,
+    clearFavoriteCurrencies
 })(FavoriteCurrencies);
--- src/reducers/currencies.js
+++ src/reducers/currencies.js
@@ -1,12 +1,57 @@
 import * as actions from '../actions';
 
 const defaultState = {
+    loading: false,
+    all: [],
+    favoriteCodes: []
 };
-
 export default (state = defaultState, action) => {
     const {type, payload} = action;
 
     switch (type) {
+        case actions.FETCH_CURRENCIES_LIST_STARTED: {
+            return {
+                ...state,
+                loading: true,
+                all: [],
+                favoriteCodes: []
+            };
+        }
+        case actions.FETCH_CURRENCIES_LIST_FINISHED: {
+            return {
+                ...state,
+                loading: false,
+                all: payload,
+                favoriteCodes: []
+            };
+        }
+        case actions.FETCH_CURRENCIES_LIST_FAILED: {
+            return {
+                ...state,
+                loading: false,
+                all: [],
+                favoriteCodes: []
+            };
+        }
+        case actions.CLEAR_FAVORITE_CURRENCIES: {
+            return {
+                ...state,
+                favoriteCodes: []
+            };
+        }
+        case actions.TOGGLE_FAVORITE_CURRENCY: {
+            if (state.favoriteCodes.includes(payload)) {
+                return {
+                    ...state,
+                    favoriteCodes: state.favoriteCodes.filter((code) => code !== payload)
+                };
+            }
+            return {
+                ...state,
+                favoriteCodes: [...state.favoriteCodes, payload]
+            };
+        }
+
         default:
             return state;
     }
